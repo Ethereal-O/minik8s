5ad0858602495ed1d6fe134fb4f00488d08911d2
yf_peng <yf_peng@sjtu.edu.cn>
2023-04-27 22:53:55 +0800

    update testRS

--- scripts/testRS.sh
+++ scripts/testRS.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# Start RS1
 ./kubectl apply -f ./yaml/rs1.yaml
-sleep 1
+sleep 30
 echo "[TestRS] RS1 started!" 1>&2
-./kubectl apply -f ./yaml/service1.yaml
-sleep 1
-echo "[TestRS] Service1 started!" 1>&2
+./kubectl get -t Pod 1>&2
+
+# Stop all Pause containers
+docker stop $(docker ps -a | grep PAUSE | awk '{print $1}')
+sleep 30
+echo "[TestRS] RS1 restored its replicas!" 1>&2
+./kubectl get -t Pod 1>&2
