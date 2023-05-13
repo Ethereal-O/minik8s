5dbc43843b76f6f977f70596d7605f3693b25f06
yf_peng <yf_peng@sjtu.edu.cn>
2023-05-13 22:33:55 +0800

    fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo killall kubectl > /dev/null 2>&1
+sleep 10
+sudo killall -9 kubectl > /dev/null 2>&1
 echo "[Cleaner] K8S Master/Worker stopped!" 1>&2
 sudo killall etcd > /dev/null 2>&1
 echo "[Cleaner] ETCD stopped!" 1>&2
