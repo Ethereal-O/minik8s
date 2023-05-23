2c75d4645b55391cb89303a5b4b318df83d09a2e
Ethereal <m15635418665@163.com>
2023-05-24 02:37:00 +0800

    feat: add multi subpath for gateway

--- scripts/testGateway.sh
+++ scripts/testGateway.sh
@@ -8,7 +8,10 @@ sleep 1
 echo "[TestGateway] Pod2 started!" 1>&2
 ./kubectl apply -f ./yaml/service1.yaml
 sleep 1
-echo "[TestGateway] Service1 started!" 1>&2 
+echo "[TestGateway] Service1 started!" 1>&2
+./kubectl apply -f ./yaml/service3.yaml
+sleep 1
+echo "[TestGateway] Service3 started!" 1>&2
 ./kubectl apply -f ./yaml/gateway1.yaml
 sleep 1
 echo "[TestGateway] Gateway1 started!" 1>&2
